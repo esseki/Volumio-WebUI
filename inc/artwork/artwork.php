3effe3362243ee717f671f7f2488d58659f0f960
esseki <esseki@gmail.com>
2014-10-20 23:16:22 +0200

    Clean the code
    Remove code corresponding to a function not used

--- inc/artwork/artwork.php
+++ inc/artwork/artwork.php
@@ -451,19 +451,6 @@ class artworkManager {
   }
 }
 
-function caching_headers ($file, $timestamp) {
-    $gmt_mtime = gmdate('r', $timestamp);
-    header('ETag: "'.md5($timestamp.$file).'"');
-    header('Last-Modified: '.$gmt_mtime);
-    header('Cache-Control: public');
-
-    if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) || isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
-        if ($_SERVER['HTTP_IF_MODIFIED_SINCE'] == $gmt_mtime || str_replace('"', '', stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) == md5($timestamp.$file)) {
-            header('HTTP/1.1 304 Not Modified');
-            exit();
-        }
-    }
-}
 
 // Handle browser caching for artwork
 cacheManager::httpCachingHeaders(__FILE__, filemtime(__FILE__));
